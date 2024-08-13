4b9df75cea86bdc4f17359a4467ad7871390dda9
Achanta28 <120081344+Achanta28@users.noreply.github.com>
2024-08-13 13:39:45 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './components/Welcome';
+
+function App() {
+  let hinMarks:Number = 20;
+  return (
+    <div className="App">
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
